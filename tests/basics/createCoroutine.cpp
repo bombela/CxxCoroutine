75da5ec0be603672243d6f8b81cf725e9611ff2c
bombela <bombela@gmail.com>
2010-07-31 00:51:46 -0700

    init

---
+++ tests/basics/createCoroutine.cpp
@@ -0,0 +1,13 @@
+/*
+ * createCoroutine.cpp
+ * Copyright © 2010 François-Xavier 'Bombela' Bourlet <bombela@gmail.com>
+ *
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "HELLO" << std::endl;
+	return 1;
+}
